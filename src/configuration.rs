ba55232cf83463ed7655b6f5d43869a03a2a34d0
Jeff Mitchell <crusty.rustacean@gmail.com>
2025-12-26 12:09:39 -0800

    bug: fix missing environment config

--- src/configuration.rs
+++ src/configuration.rs
@@ -50,6 +50,7 @@ impl DatabaseSettings {
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let base_path = std::env::current_dir().expect("Failed to determine the current directory");
     let configuration_directory = base_path.join("configuration");
+
     // Detect the running environment.
     // Default to `local` if unspecified.
     let environment: Environment = std::env::var("APP_ENVIRONMENT")
@@ -64,14 +65,24 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
         .add_source(config::File::from(
             configuration_directory.join(environment_filename),
         ))
+        // Add in settings from environment variables (with a prefix of APP and '__' as separator)
+        // E.g. `APP_APPLICATION__PORT=5001 would set `Settings.application.port`
+        .add_source(
+            config::Environment::with_prefix("APP")
+                .prefix_separator("_")
+                .separator("__"),
+        )
         .build()?;
+
     settings.try_deserialize::<Settings>()
 }
+
 /// The possible runtime environment for our application.
 pub enum Environment {
     Local,
     Production,
 }
+
 impl Environment {
     pub fn as_str(&self) -> &'static str {
         match self {
@@ -80,15 +91,16 @@ impl Environment {
         }
     }
 }
+
 impl TryFrom<String> for Environment {
     type Error = String;
+
     fn try_from(s: String) -> Result<Self, Self::Error> {
         match s.to_lowercase().as_str() {
             "local" => Ok(Self::Local),
             "production" => Ok(Self::Production),
             other => Err(format!(
-                "{} is not a supported environment. \
-Use either `local` or `production`.",
+                "{} is not a supported environment. Use either `local` or `production`.",
                 other
             )),
         }
